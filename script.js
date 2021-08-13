f9b9955bd7c8c36560694b6eb094826971ab7e26
ALAE <57995821+ALAAEddineACHEACHE@users.noreply.github.com>
2021-08-13 15:44:29 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,9 +1,55 @@
-let nav = document.getElementsByTagName("nav");
-let boutton = document.getElementById("toggle");
-let body = document.getElementById("body");
-boutton.addEventListener("click",function(event){
+let A = document.getElementById("A");
+let S = document.getElementById("S");
+let D = document.getElementById("D");
+let F = document.getElementById("F");
+let G =document.getElementById("G");
+let H = document.getElementById("H");
+let J = document.getElementById("J");
+let K = document.getElementById("K");
+let L = document.getElementById("L");
+let firstaudio= document.getElementById("a");
+let secondaudio = document.getElementById("s");
+let thirdaudio = document.getElementById("d");
+let fourthaudio=document.getElementById("f")
+let fifthaudio = document.getElementById("g");
+let sixthaudio = document.getElementById("h");
+let seventhaudio=document.getElementById("j");
+let eighthaudio=document.getElementById("k");
+let ninethaudio=document.getElementById("l");
+A.addEventListener("keydown",function(event){
     event.preventDefault();
-    console.log(body);
-    body.classList.toggle("toggles");
+    console.log(event.key);
+    firstaudio.play();
 });
+S.addEventListener("click",function(){
+    secondaudio.play();
+})
+D.addEventListener("click",function(){
+    thirdaudio.play();
+})
+F.addEventListener("click",function(){
+    fourthaudio.play();
+})
+G.addEventListener("click",function(){
+    fifthaudio.play();
+})
+H.addEventListener("click",function(){
+    sixthaudio.play();
+})
+J.addEventListener("click",function(){
+    seventhaudio.play();
+})
+K.addEventListener("click",function(){
+    eighthaudio.play();
+})
+L.addEventListener("click",function(){
+    ninethaudio.play();
 
+ });
+ //Deuxieme Methode l'écoute du clavier en utlisant le convertissement 
+ //des codes ASCI II
+window.addEventListener("keydown",function(event){
+    console.log(event.keyCode);
+    const currentAudio = document.querySelector(`audio[data-key="${event.keyCode}"]`);
+    currentAudio.play();
+})
